704956981781cbf4bc7b7e2ce65f1637acc05146
SarahRWarren <54514764+SarahRWarren@users.noreply.github.com>
2021-09-06 20:24:37 -0400

    we did it, kids

--- make-figs.R
+++ make-figs.R
@@ -155,8 +155,6 @@ ggplot(user_time, aes(x=ym, y=mean_length, fill=n_posts)) +
 
 ggsave("figs/ip_length_frequency.png", width=20)
 
-
-
 ggplot(user_time, aes(x=ym, y=n_posts, fill=mean_length)) + 
   geom_col() + theme_bw() +
   theme(axis.text.x = element_text(angle = 90)) +
@@ -176,3 +174,83 @@ ggplot(user_time, aes(x=ym, y=n_posts, fill=mean_length)) +
        fill = "Average Length")
 
 ggsave("figs/ip_frequency_length.png", width=20)
+
+
+########## all together now
+all <- rbind(ladies, ip)
+write_rds(all, "data/all_scraped.Rds")
+
+user_rank <- all %>%
+  group_by(user) %>%
+  summarise(n = n()) %>%
+  subset(n > 1)
+
+##make figs
+ggplot(user_rank, aes(x=n, y=user)) + 
+  geom_col() + theme_bw() +
+  theme(axis.text = element_text(size=2.5)) +
+  scale_x_continuous(limit=c(0,600),
+                     breaks = c(0, 10, 20, 30, 40, 50, 100, 150, 200,
+                                250, 300, 350, 400, 450, 500, 550, 600),
+                     expand = c(0,0)) +
+  labs(title = "Posts by User",
+       caption = "In All Scraped Forums",
+       x= "Number of Posts",
+       y="Username")
+ggsave("figs/user_rank_all.png", height=20)
+
+
+####################################
+user_time <- all %>% 
+  separate(date,
+           into = c('m', 'd', 'y'),
+           sep = '-',
+           remove = F) %>% 
+  mutate(date = as.Date(ISOdate(y, m, d)),
+         ym = paste0(y, "-", m)) %>% 
+  group_by(user, ym) %>% 
+  add_count(user, 
+            name = "n_posts") %>% 
+  summarise(mean_length = mean(length),
+            n_posts = mean(n_posts)) %>% 
+  arrange(ym) %>% 
+  ungroup()
+
+ggplot(user_time, aes(x=ym, y=mean_length, fill=n_posts)) + 
+  geom_col() + theme_bw() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  geom_vline(xintercept = "2009-01", linetype="dashed", 
+             color = "gray", size=1.5) +
+  geom_vline(xintercept = "2013-01", linetype="dashed", 
+             color = "gray", size=1.5) +
+  geom_vline(xintercept = "2017-01", linetype="dotted", 
+             color = "red", size=1.5) +
+  geom_vline(xintercept = "2017-08", linetype="dotted", 
+             color = "red", size=1.5) +
+  geom_vline(xintercept = "2021-01", linetype="dashed", 
+             color = "gray", size=1.5) +
+  labs(title = "Posts Per Month-Year",
+       x= "Year-Month",
+       y="Average Length",
+       fill = "Number of Posts")
+
+ggsave("figs/all_length_frequency.png", width=20)
+
+ggplot(user_time, aes(x=ym, y=n_posts, fill=mean_length)) + 
+  geom_col() + theme_bw() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  geom_vline(xintercept = "2009-01", linetype="dashed", 
+             color = "gray", size=1.5) +
+  geom_vline(xintercept = "2013-01", linetype="dashed", 
+             color = "gray", size=1.5) +
+  geom_vline(xintercept = "2017-01", linetype="dotted", 
+             color = "red", size=1.5) +
+  geom_vline(xintercept = "2017-08", linetype="dotted", 
+             color = "red", size=1.5) +
+  geom_vline(xintercept = "2021-01", linetype="dashed", 
+             color = "gray", size=1.5) +
+  labs(title = "Posts Per Month-Year",
+       x= "Year-Month",
+       y="Number of Posts",
+       fill = "Average Length")
+ggsave("figs/all_frequency_length.png", width=20)
