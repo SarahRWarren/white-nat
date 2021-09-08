363531bd0cb4a6f1de49f11f7e463232799ac82b
SarahRWarren <54514764+SarahRWarren@users.noreply.github.com>
2021-09-08 13:58:36 -0400

    attempting text analysis (needs more cleaning)

--- attempting-text-analysis.R
+++ attempting-text-analysis.R
@@ -5,6 +5,10 @@ library(qdap)
 library(Rcpp)
 data('Top200Words')
 
+####more cleaning needed, better cleaning needed
+
+
+
 ladies <- read_rds("data/ladies.Rds")
 
 unigram <- ladies %>% 
@@ -35,11 +39,11 @@ unigram <- ladies %>%
   anti_join(stop_words)
 
 #Add the word stemmed words. 
-unigram <- unigram %>% 
-  mutate(word_stem = wordStem(word),
-         word_stem = ifelse(word == 'baby' | word == 'mother' | word == 'pregnant',
-                            as.character(word),
-                            word_stem))
+#unigram <- unigram %>% 
+#  mutate(word_stem = wordStem(word),
+#         word_stem = ifelse(word == 'baby' | word == 'mother' | word == 'pregnant',
+#                            as.character(word),
+#                            word_stem))
 
 #Create a word count dataframe
 unigram_count <- unigram %>% 
@@ -49,8 +53,36 @@ unigram_count <- unigram %>%
          word_stem = ifelse(word == 'baby' | word == 'mother' | word == 'pregnant',
                             word,
                             word_stem))
+###sort
+unigram_sorted <- unigram %>% 
+  count(word, sort = T) %>% 
+  mutate(word = reorder(word, n))
+
+unigram_200 <- unigram_sorted %>%
+  subset(n >= 98)
+
+ggplot(unigram_200, aes(x=n, y=word, fill=n)) + geom_col() + theme_bw() +
+  scale_x_continuous(limit=c(0,4000),
+                     breaks = c(0, 100, 150, 200, 250, 300, 400, 500,
+                                600, 700, 800, 900, 1000,
+                                1100, 1200, 1300, 1400, 1500,
+                                2000, 3000, 3500),
+                     expand = c(0,0)) +
+  theme(axis.text = element_text(size=5)) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(title = "Word Frequency on Women's Forums",
+       x= "Freqeuncy",
+       y="",
+       fill = "Frequency (High-Low)")
+ggsave("figs/unigram_ladies_200.png", height=12, width = 12)
+
+
+
 
 ####################
+
+
+##Still having cleaning issues here
 bigram <- ladies %>% 
   mutate(text_clean = str_replace_all(text_nopunct, 
                                       "[Nn]ational\\s[Ss]ocialism[A-z]*", 
@@ -59,8 +91,8 @@ bigram <- ladies %>%
                            "ns") %>% 
            str_replace_all("[Pp]regnant[A-z]*",
                            "pregnant") %>% 
-           str_replace_all("[Hh]itlers*",
-                           "hitler") %>%  
+           str_replace_all("[Pp]regnant\\s[Tt]read[A-z]*",
+                           "pregnant thread") %>%  
            str_replace_all("[A-z]*[Mm]ein(\\s)*[Kk]ampf[A-z]*",
                            "meinkampf") %>% 
            str_replace_all("[Nn]ationalis[tm]",
@@ -98,6 +130,4 @@ bigram_sorted <- bigram %>%
   count(bigram, sort = T) %>% 
   mutate(bigram = reorder(bigram, n))
 
-ggplot(bigram_sorted, aes(x=bigram, y=n)) + geom_col() + coord_flip() +
-  theme_bw()
-  
+bigram_sorted <- rename(bigram_sorted, n_freq = n)
