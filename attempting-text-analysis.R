5e5a4ef112a77eca24dbdcfa80a1a9cc1582af2d
SarahRWarren <54514764+SarahRWarren@users.noreply.github.com>
2021-09-09 16:36:43 -0400

    afinn

--- attempting-text-analysis.R
+++ attempting-text-analysis.R
@@ -24,12 +24,8 @@ unigram <- ladies %>%
                            "pregnant") %>% 
            str_replace_all("[Hh]itlers*",
                            "hitler") %>%  
-           str_replace_all("[A-z]*[Mm]ein(\\s)*[Kk]ampf[A-z]*",
-                           "meinkampf") %>% 
            str_replace_all("[Nn]ationalis[tm]",
                            "national") %>% 
-           str_replace_all("[A-z]*([Tt]hird|3rd)(\\s)*[Rr]eich[A-z]*",
-                           "3rd_reich") %>% 
            str_replace_all("[Nn]azi",
                            "nazi")) %>%
   unnest_tokens(word, 
@@ -122,3 +118,40 @@ unigram_afinn <- inner_join(unigram, get_sentiments("afinn"))  %>%
          value,
          net_score) %>% 
   inner_join(user_rank)
+
+###plot sentiment frequency
+ggplot(unigram_nrc_user, aes(x=sentiment, fill=sentiment)) +
+  geom_bar() + theme_bw() +
+  scale_fill_manual(values = c("#FF0000", "#FFA500", "#89a203",
+                      "#A020F0", "#FFFF00", "#000080",
+                      "#ADD8E6", "#0000FF", "#228B22",
+                      "#FFC0CB")) +
+  labs(title = "NRC Sentiment Frequency in Women's Posts",
+       x = "",
+       y = "Frequency",
+       fill = "NRC Sentiment")
+ggsave("figs/sentiment_freq_ladies.png", width=8)
+
+
+unigram_nrc_user <- unigram_nrc_user %>%
+  mutate(date = as.Date(date, "%m-%d-%Y"))
+
+ggplot(unigram_nrc_user, aes(x=date, fill=sentiment)) + 
+  geom_density(alpha=0.5) + theme_bw() +
+  scale_fill_manual(values = c("#FF0000", "#FFA500", "#89a203",
+                               "#A020F0", "#FFFF00", "#000080",
+                               "#ADD8E6", "#0000FF", "#228B22",
+                               "#FFC0CB")) +
+  scale_y_continuous() +
+  labs(title = "NRC Sentiment Frequency in Women's Posts",
+       x = "Date",
+       y = "Density",
+       fill = "NRC Sentiment") + facet_grid(vars(sentiment))
+ggsave("figs/sentiment-over-time-women.png", width = 10, height = 10)
+  
+ggplot(unigram_afinn, aes(x=date, y=value)) + geom_col() +
+  theme_bw() + scale_y_continuous() +
+  labs(title = "Female AFINN Sentiment Analysis Over Time",
+       x = "Date",
+       y = "AFINN Polarity Score")
+ggsave("figs/afinn-over-time-women.png")
